04ab65179d4426caef5bd83cf06db3f6b1e9b147
Yash Garde <36286221+yashgarde@users.noreply.github.com>
2025-01-22 15:05:56 -0800

    adding fill_array.c

---
+++ fill_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void fill(int *parray, int length) {
+    parray = malloc(length * sizeof(int));
+    for (int i = 0; i < length; i++) {
+        parray[i] = 10 * (i + 1);
+    }
+}
+
+int main() {
+    int *parray = NULL;
+    fill(parray, 3);
+    for (int i = 0; i < 3; i++) {
+        printf("%d\n", parray[i]);
+    }
+    return 0;
+}
