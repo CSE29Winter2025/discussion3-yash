ab7f6edeae503906120f23450609e1fb5e053fc8
Yash Garde <ygarde@ucsd.edu>
2025-01-22 16:47:36 -0800

    fill fixed

--- fill_array.c
+++ fill_array.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fill(int *parray, int length) {
-    parray = malloc(length * sizeof(int));
+void fill(int **parray, int length) {
+    *parray = malloc(length * sizeof(int));
     for (int i = 0; i < length; i++) {
-        parray[i] = 10 * (i + 1);
+        (*parray)[i] = 10 * (i + 1);
     }
 }
 
 int main() {
-    int *parray = NULL;
-    fill(parray, 3);
+    int *parray = NULL; 
+    fill(&parray, 3);
     for (int i = 0; i < 3; i++) {
         printf("%d\n", parray[i]);
     }
